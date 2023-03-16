61ecb3b0a56ea0a0be74f8bb082c80b29a1f9465
sovereign-stone <shijiuyi@socmap.net>
2023-03-16 15:20:28 +0800

    init train learn

---
+++ train_learn/set_nltk.py
@@ -0,0 +1,16 @@
+import nltk
+
+
+def nltk_download():
+    nltk.set_proxy('http://127.0.0.1:58591')  # 设置代理，如果需要的话
+    nltk.download('all', download_dir='/home/soc/nltk_data', quiet=False,
+                  raise_on_error=True)
+
+
+def test():
+    from nltk.corpus import brown
+    words = brown.words()
+    print(words)
+
+
+# nltk_download()
